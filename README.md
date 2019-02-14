2f877a438290f195145f0ab917dfcf5aae2c0fa5
Marcia Silva <contato.pix@gmail.com>
2019-02-14 19:24:20 -0200

    Componentes para manipulação de placar

---
+++ README.md
@@ -0,0 +1,3 @@
+Curso Básico de React
+=====================
+Código desenvolvido durante as aulas nos vídeos do [Canal V++](https://youtube.com/user/VPlusPlus).
---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PlacarContainer from './PlacarContainer';
+
+const dados = {
+    partida: {
+        estadio: 'Maracanã',
+        data: '15/11/2019',
+        horario: '19h'
+    },
+    casa: {
+        nome: "Vasco"
+    },
+    visitante: {
+        nome: "Flamengo"
+    }
+}
+
+export default class App extends React.Component{
+    render(){
+        return <PlacarContainer partida={dados.partida} casa={dados.casa} visitante={dados.visitante} />;
+    }
+}
